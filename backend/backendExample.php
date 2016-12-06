445d0b19bb6bd31846ed2dbd8d06d5eb0c5c1318
vlopes770 <vlopes770@gmail.com>
2016-12-05 23:15:07 -0800

    Add files via upload

--- backend/backendExample.php
+++ backend/backendExample.php
@@ -13,7 +13,7 @@
 		{
 			if($query == 'details')
 			{
-				echo("returns details");
+				echo("returns brand details");
 			}
 			if($query=='candesigns')
 			{
@@ -24,6 +24,55 @@
 				echo("returns flavors");
 			}
 		}
+		if($uriType=='cans')
+		{
+			if($query=='details')
+			{
+				echo("returns can details");
+			}
+			
+		}
+		if($uriType=='companies')
+		{
+			if($query=='details')
+			{
+				echo("returns company details");
+			}
+			if($query=='brands')
+			{
+				echo("returns brands of the company");
+			}
+		}
+		if($uriType=='flavors')
+		{
+			if($query=='details')
+			{
+				echo("returns flavor details");
+			}
+			if($query=='cans')
+			{
+				echo("returns cans of the flavor");
+			}
+			if($query=='brands')
+			{
+				echo("returns flavors of the brand");
+			}
+			if($query=='companies')
+			{
+				echo("returns flavors of the company");
+			}
+		}
+	}
+	if($ver=="POST")
+	{
+		if($uriType=="cans")
+		{
+			if($query=='userReview')
+			{
+				echo("posts user review");
+			}
+		}
+	}
 		
 		/*echo($_SERVER['DOCUMENT_ROOT']);
 		$query = $_GET['name'];
